7911b4be12a70a816336a9adcc7e25810f7a972c
amirM <amir.mhe83@gmail.com>
2025-02-27 23:56:01 +0330

    preparing Maven and loginPage

--- src/main/java/mft/library/Main.java
+++ src/main/java/mft/library/Main.java
@@ -1,7 +1,15 @@
 package mft.library;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
     }
 }

c16f0bc97702cfaa7aedb8e7ff5e6ae61802b8b1
amirM <amir.mhe83@gmail.com>
2025-03-04 18:32:09 +0330

    updating the bank Database

--- src/main/java/bank/app/model/repository/dbsql/bank_db.sql
+++ src/main/java/bank/app/model/repository/dbsql/bank_db.sql
@@ -1,19 +1,27 @@
-CREATE TABLE Accounts
+create table accounts
 (
-    id number primary key
+    id number primary key,
+    account_type  nvarchar2(15) not null,
+    balance number
 );
 
 
-CREATE TABLE Cheques
+create table cheques
 (
-    id number primary key
+    id number primary key,
+    cheque_address nvarchar2(12),
+    status nvarchar2(12)
 
 );
 
 
 CREATE TABLE transactions
 (
-    id number primary key
+    id number primary key,
+    sender_id number,
+    receive_id number,
+    amount number,
+    timestamp date
 
 );
 
@@ -29,4 +37,11 @@ create table users
     username   varchar2(30) unique,
     password   varchar2(16)  not null,
     is_active  number(1) default 1
-)
+);
+
+create table card
+(
+    card_number number,
+    cvv2 number,
+    expiry_date date
+);
